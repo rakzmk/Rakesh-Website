b079b46f8d3fdbf8f8ea3c1f054a318d2580dd4f
Rakesh MK <rakzmk@gmail.com>
2021-01-24 04:03:48 +0530

    last2

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 50) {
-        $('.bg2').css('background', '#000');
+    if ($(window).scrollTop() >= 600) {
+        $('.bg3').css('background', '#000');
     } else {
-        $('.bg2').css('background', 'transparent');
+        $('.bg3').css('background', 'transparent');
     }
 });
